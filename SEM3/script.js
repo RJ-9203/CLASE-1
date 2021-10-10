07f61fe31b3d4b9841b6a83f77bb09735895ab27
Rolnan <92193345+RJ-9203@users.noreply.github.com>
2021-10-09 20:33:17 -0500

    proyecto 3

---
+++ SEM3/script.js
@@ -0,0 +1,41 @@
+  // comentario en una linea
+
+  /* cokentarios
+  de múltiples
+  lineas
+  */
+ // asignación de datos, números no llevan comillas
+ var nombre = "Rolnan";
+
+ var apellido = "Gil";
+ // 
+alert(nombre);
+alert(apellido);
+// arrays, se usan corchetes
+
+var dia1 = "Lunes";
+var dia1 = "Martes";
+var dia1 = "Miercoles";
+var dia1 = "Jueves";
+var dia1 = "Viernes";
+var dia1 = "Sabado";
+var dia1 = "Domingo";
+
+
+var dias = ["Lunes","Martes","Miercoles","Jueve","Viernes","Sabado","Domingo"];
+
+// empieza desde posición 0
+
+alert(dias[5]);
+
+//ejercicio
+
+var dato1 = "Rolnan";
+var dato2 = "Gil";
+var dato3 = 29;
+var dato4 = "rogil_92@outlook.com";
+
+var datos = ["Rolnan", "Gil", "29", "rogil_92@outlook.com"];
+
+alert(datos[2]);
+
